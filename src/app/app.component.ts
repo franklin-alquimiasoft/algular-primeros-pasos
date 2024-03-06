f0aafb0949270a43f45d93aa8921e41aba1e5bbc
Franklin L <franklinl@alquimiasoft.com.ec>
2024-03-06 00:06:32 -0500

    nuevo

---
+++ src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { __values } from 'tslib';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  public title = 'Hola Mundo';
+}
---
+++ src/app/dbz/components/list/list.component.ts
@@ -0,0 +1,24 @@
+import { Component, EventEmitter, Input, Output, input } from '@angular/core';
+import { Character } from '../../interfaces/character.interface';
+
+@Component({
+  selector: 'dbz-list',
+  templateUrl: './list.component.html',
+  styleUrl: './list.component.css',
+})
+export class ListComponent {
+  @Input()
+  public characterList: Character[] = [{
+    name: 'Trunk',
+    power: 10
+  }];
+
+  @Output()
+  public onDelete: EventEmitter<string> = new EventEmitter();
+
+  onDeleteCharacterById(id?: string): void {
+    if (!id) return;
+
+    this.onDelete.emit(id);
+  }
+}
